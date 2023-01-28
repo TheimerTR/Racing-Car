91a4ac50f8756fa371364cf54a365f88376829fe
SoniaOL <99950351+SoniaOL@users.noreply.github.com>
2023-01-28 02:30:55 +0100

    intento fallido de poner una curva :(

--- Racing car/ModuleSceneIntro.cpp
+++ Racing car/ModuleSceneIntro.cpp
@@ -20,23 +20,45 @@ bool ModuleSceneIntro::Start()
 	App->camera->Move(vec3(1.0f, 1.0f, 0.0f));
 	App->camera->LookAt(vec3(0, 0, 0));
 
-	Cylinder* c = new Cylinder();
-	c->height = 1.5f;
-	c->radius = 0.5f;
-	c->SetPos(0, 0.8f, 0); 
-	c->SetRotation(90, vec3(0,0,1));
-	App->physics->AddBody(*c, 0); 
-
-
-	primitives.PushBack(c); 
-
-	
-
-	for (int n = 0; n < primitives.Count(); n++) {
-
-
+	int posx = 0;
+	for (int n = 0; n < 10; n++) {
+		Cylinder* c = new Cylinder();
+		c->height = 1.5f;
+		c->radius = 0.5f;
+		c->SetPos(posx, 0.8f, 0);
+		c->SetRotation(90, vec3(0, 0, 1));
+		App->physics->AddBody(*c, 0);
+		primitives.PushBack(c);
+		posx = posx + 2;
 	}
-	
+
+	/*vec3 initial = { -3, 0.8f, -1 };
+	vec3 final = { -30, 0.8f, -50 };
+	float radius = 0.5f;
+	float PI = 3.14159;
+
+		float deltax = final.x - initial.x;
+		float deltaz = final.z - initial.z;
+		float distance = sqrt((deltax * deltax) + (deltaz * deltaz));
+		float increment = distance / (10 - 1);
+		float angle = atan2(deltaz, deltax);
+		for (int i = 0; i < 10; i++) {
+			float x = initial.x + i * increment * cos(angle);
+			float z = initial.z + i * increment * sin(angle);
+			float y = 0.0;
+
+
+			Cylinder* c = new Cylinder();
+			c->height = 1.5f;
+			c->radius = 0.5f;
+			c->SetPos(x, y, z);
+			c->SetRotation(90, vec3(0, 0, 1));
+			App->physics->AddBody(*c, 0);
+			primitives.PushBack(c);
+
+			LOG("x: %f, z: %f, angle: %f", x, z, angle);
+		}
+	*/
 
 	return ret;
 }
